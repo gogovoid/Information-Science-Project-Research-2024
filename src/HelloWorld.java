d73f561bc2aef4ed7cd9dc4260a475fd3e73bc70
gogovoid <vcloud9@gmail.com>
2024-03-07 21:47:17 +0900

    v0.1 hello

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 // alt + shift + F10
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World~");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Hello " + name + "!");
     }
 }
