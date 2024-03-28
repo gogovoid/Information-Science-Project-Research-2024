82739b12e6e82dd36d7e5832a39dd205772cf8f8
gogovoid <vcloud9@gmail.com>
2024-03-28 21:07:54 +0900

    v1.3 pokemon game

--- src/pokemons/Charizard.java
+++ src/pokemons/Charizard.java
@@ -11,9 +11,4 @@ public class Charizard extends Pokemon implements Flyable{
     public void attack(Pokemon targetPokemon) {
         System.out.println(this.getName() + " fires a flamethrower attack at "+ targetPokemon.getName() +".");
     }
-
-    @Override
-    public void fly() {
-        System.out.println(this.getName() + " flies through the sky with its wings.");
-    }
 }
